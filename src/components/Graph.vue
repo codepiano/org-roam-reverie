0473a136fddd3f972f1297a4435ebfb756f44620
codepiano <codepiano.li@gmail.com>
2021-07-21 01:23:03 +0800

    remove cytoscape

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -5,48 +5,40 @@
 <script>
 
 import cytoscape from 'cytoscape'
+import cola from 'cytoscape-cola'
 import {getData} from '@/js/axios'
 
 export default {
   name: 'Graph',
   methods: {
     createCytoscape() {
-      cytoscape.warnings(false);
+
+      cytoscape.use(cola);
       const cy = cytoscape({
         container: document.getElementById('graph'),
         boxSelectionEnabled: false,
-        userZoomingEnabled: false, //滚轮缩放
-        wheelSensitivity: 0.1,
-        autounselectify: false,
-        autoungrabify: true,
+        userZoomingEnabled: true, //滚轮缩放
         layout: {
-          name: 'breadthfirst',
+          name: 'cola',
+          nodeDimensionsIncludeLabels: true,
         },
-        minZoom: 0.3,
         style: [{
           selector: 'node',
           style: {
-            'content': 'data(id)',
-            'text-opacity': 0.5,
-            "height": 40,
-            "width": 40,
+            'content': 'data(title)',
             'pie-size': '100%',
+            'text-wrap': "wrap",
+            'text-max-width': "150",
+            'text-overflow-wrap': "anywhere",
+            'text-justification': "center",
             'text-valign': 'center',
             'text-halign': 'left',
           }
         },
           {
-            selector: ':parent',
-            css: {
-              'text-valign': 'top',
-              'text-halign': 'center',
-            }
-          },
-          {
             selector: 'edge',
             style: {
               width: 3,
-              label: 'data(label)',
               'target-arrow-shape': 'triangle',
               'line-color': '#9dbaea',
               'target-arrow-color': '#9dbaea',
